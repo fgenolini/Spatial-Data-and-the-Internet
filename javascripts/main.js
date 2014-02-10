bbac07d027314dc9e0b66e74a1f765a0525025af
François Génolini <fgenolini@hotmail.com>
2014-02-10 23:14:14 +0000

    jQuery 2.1 does not work with IE10
    Reverting to jQuery 1.11

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@
 Home page of module A1136 Spatial Data and the Internet, Cranfield University (UK)
  */
 if(typeof jQuery === 'undefined'){
-    document.write(unescape("%3Cscript src='javascripts/jquery-2.1.0.min.js' type='text/javascript'%3E%3C/script%3E"));
+    document.write(unescape("%3Cscript src='javascripts/jquery-1.11.0.min.js' type='text/javascript'%3E%3C/script%3E"));
 }
 $(document).ready(function () {
 });
