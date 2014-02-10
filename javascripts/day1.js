bc120cc2136162e256b7d61e164178b4e2d1c721
François Génolini <fgenolini@hotmail.com>
2014-02-10 23:08:04 +0000

    Added missing JavaScripts
    Day 1 and Isle of Wight HTML had missing JavaScripts

--- javascripts/day1.js
+++ javascripts/day1.js
@@ -1,5 +1,5 @@
 /*
-Home page of module A1136 Spatial Data and the Internet, Cranfield University (UK)
+Day 1 of module A1136 Spatial Data and the Internet, Cranfield University (UK)
  */
 if(typeof jQuery === 'undefined'){
     document.write(unescape("%3Cscript src='javascripts/jquery-2.1.0.min.js' type='text/javascript'%3E%3C/script%3E"));
