e2eb498dfab1e720f5378dc83da3c704397bd93d
François Génolini <fgenolini@hotmail.com>
2014-02-11 15:07:10 +0000

    Added missing files, highlighted hot zones on picture

---
+++ javascripts/day2.js
@@ -0,0 +1,8 @@
+/*
+Day 2 of module A1136 Spatial Data and the Internet, Cranfield University (UK)
+ */
+if(typeof jQuery === 'undefined'){
+    document.write(unescape("%3Cscript src='javascripts/jquery-1.11.0.min.js' type='text/javascript'%3E%3C/script%3E"));
+}
+$(document).ready(function () {
+});
