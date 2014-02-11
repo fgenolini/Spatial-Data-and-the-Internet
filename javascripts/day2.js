fb0c3add11d2f35f7be3be36b47578e891d66e0d
François Génolini <fgenolini@hotmail.com>
2014-02-11 16:03:15 +0000

    Added simple google map to day 2 page

--- javascripts/day2.js
+++ javascripts/day2.js
@@ -5,4 +5,17 @@ if(typeof jQuery === 'undefined'){
     document.write(unescape("%3Cscript src='javascripts/jquery-1.11.0.min.js' type='text/javascript'%3E%3C/script%3E"));
 }
 $(document).ready(function () {
+    var googleMap;
+    function googleMapInitialize() {
+        var mapOptions = {
+            center: new google.maps.LatLng(-34.397, 150.644),
+            zoom: 8
+        };
+        googleMap = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+    }
+
+    googleMapInitialize();
+    google.maps.event.trigger(googleMap, 'resize');
+
 });
