0271300c2956651d2e7718d335a913f107590566
François Génolini <fgenolini@hotmail.com>
2014-02-15 17:26:26 +0000

    dbg...

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,49 +5,52 @@ if (typeof jQuery === 'undefined') {
     document.write(unescape("%3Cscript src='javascripts/jquery-1.11.0.min.js' type='text/javascript'%3E%3C/script%3E"));
 }
 $(document).ready(function () {
-    var course_progression;
-    $.getJSON("course_progression.geojson", function (result) {
-        course_progression = result;
-    });
+    function setupLeaflet(result) {
+        var course_progression = result;
 
-    var world = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/22677/256/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-        key: 'BC9A493B41014CAABB98F0471D759707'
-    });
+        var world = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/22677/256/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
+            key: 'BC9A493B41014CAABB98F0471D759707'
+        });
 
-    var jsonStyle = {
-        "color": "#ff7800",
-        "weight": 5,
-        "opacity": 0.65
-    };
+        var jsonStyle = {
+            "color": "#ff7800",
+            "weight": 5,
+            "opacity": 0.65
+        };
 
-    var jsonOptions = {
-        style: function (feature) {
-            console.log("JSON Feature: " + feature);
-            switch (feature.properties.meta_type) {
-                case 'plan': return { color: "#ff0000" };
-                case 'lecture': return { color: "#0000ff" };
+        var jsonOptions = {
+            style: function (feature) {
+                console.log("JSON Feature: " + feature);
+                switch (feature.properties.meta_type) {
+                    case 'plan': return { color: "#ff0000" };
+                    case 'lecture': return { color: "#0000ff" };
+                }
             }
-        }
-    };
+        };
 
-    console.log("JSON from GitHub: (" + course_progression + ")");
-    var progression_json = L.geoJson(course_progression, jsonOptions);
+        console.log("JSON from GitHub: (" + course_progression + ")");
+        var progression_json = L.geoJson(course_progression, jsonOptions);
 
-    var mapOptions = {
-        center: new L.LatLng(3.5, -2.2),
-        zoom: 10,
-        layers: [world, progression_json]
-    };
-    var progression_map = L.map('progression_map', mapOptions);
+        var mapOptions = {
+            center: new L.LatLng(3.5, -2.2),
+            zoom: 10,
+            layers: [world, progression_json]
+        };
+        var progression_map = L.map('progression_map', mapOptions);
 
-    var baseLayers = {
-        "World": world
-    };
+        var baseLayers = {
+            "World": world
+        };
 
-    var overlays = {
-        "Progression": progression_json
-    };
+        var overlays = {
+            "Progression": progression_json
+        };
 
-    L.control.layers(baseLayers, overlays).addTo(progression_map);
+        L.control.layers(baseLayers, overlays).addTo(progression_map);
+    }
+
+    $.getJSON("course_progression.geojson", function (result) {
+        setupLeaflet(result);
+    });
 });
