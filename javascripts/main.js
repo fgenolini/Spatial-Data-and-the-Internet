63374662d2012042e9ae3b5f18324011a571398b
François Génolini <fgenolini@hotmail.com>
2014-02-15 16:24:09 +0000

    Add background map

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,7 @@
 /*
 Home page of module A1136 Spatial Data and the Internet, Cranfield University (UK)
  */
-if(typeof jQuery === 'undefined'){
+if (typeof jQuery === 'undefined') {
     document.write(unescape("%3Cscript src='javascripts/jquery-1.11.0.min.js' type='text/javascript'%3E%3C/script%3E"));
 }
 $(document).ready(function () {
@@ -12,5 +12,9 @@ $(document).ready(function () {
 
     var progression_map = L.map('progression_map').setView([35, -2.2], 4);
 
+    L.tileLayer('http://{s}.tile.cloudmade.com/{key}/22677/256/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2012 CloudMade',
+        key: 'BC9A493B41014CAABB98F0471D759707'
+    }).addTo(progression_map);
     L.geoJson(course_progression).addTo(progression_map);
 });
