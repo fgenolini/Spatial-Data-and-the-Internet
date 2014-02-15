96586aed6ca7cc95f7f9515ca19e44f6303e5680
François Génolini <fgenolini@hotmail.com>
2014-02-15 17:39:07 +0000

    nqcy

--- javascripts/main.js
+++ javascripts/main.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
         var progression_json = L.geoJson(course_progression, jsonOptions);
 
         var mapOptions = {
-            center: new L.LatLng(-2.0, 6),
+            center: new L.LatLng(-2.8, 6),
             zoom: 7,
             layers: [world, progression_json]
         };
