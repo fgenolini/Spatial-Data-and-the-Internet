c963c32f5f70ba507e371810f5663fc4bee3ae74
François Génolini <fgenolini@hotmail.com>
2014-02-15 16:06:53 +0000

    Adding Leaflet

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,4 +5,8 @@ if(typeof jQuery === 'undefined'){
     document.write(unescape("%3Cscript src='javascripts/jquery-1.11.0.min.js' type='text/javascript'%3E%3C/script%3E"));
 }
 $(document).ready(function () {
+    var course_progression;
+    $.getJSON("course_progression.geojson", function (result) {
+        console.log(result);
+    });
 });
