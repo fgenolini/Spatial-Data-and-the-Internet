b10b221c94514263207c695c60bc5561a879853b
Francois Genolini <fgenolini@hotmail.com>
2014-02-17 11:28:57 +0000

    try again

--- javascripts/day4.js
+++ javascripts/day4.js
@@ -10,20 +10,15 @@ if (typeof jQuery === 'undefined') {
 
 $(document).ready(function () {
     var dojoConfig = { parseOnLoad: true };
+    var fgenoliniAitanaArcGisOnlineMapId = "e297c36a60424a0181ad64572ffaaaf9";
     var arcgis_map;
     require([
         "esri/map",
         "esri/arcgis/utils",
         "esri/dijit/Legend",
         "dojo/domReady!"
-    ], function (Map, arcgisUtils) {
-        //var esriMapOptions = {
-        //    center: [-0.268542, 38.638987],
-        //    zoom: 12,
-        //    basemap: "streets"
-        //};
-        //var map = new esri.Map("map", esriMapOptions);
-        new esri.Map("e297c36a60424a0181ad64572ffaaaf9", "arcgis_map").then(function (response) {
+    ], function (Map, arcgisUtils, Legend) {
+        arcgisUtils.createMap(fgenoliniAitanaArcGisOnlineMapId, "arcgis_map").then(function (response) {
             arcgis_map = response.map;
             var legend = new Legend({
                 map: arcgis_map,
@@ -34,19 +29,10 @@ $(document).ready(function () {
         });
     });
 
-    //var djConfig = { parseOnLoad: true };
-    //dojo.require("dijit.layout.BorderContainer");
-    //dojo.require("dijit.layout.ContentPane");
-    //dojo.require("esri.map");
-    //dojo.require("esri.arcgis.utils");
-    //dojo.require("dojo.domReady!");
-    //dojo.require("esri.layers.WMSLayer");
-
     function init() {
-
         // Base map showing cultural information
         // Public ESRI base map layer
-        var esriPublicServer = "http://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer";
+        //var esriPublicServer = "http://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer";
         //var baseMap = new esri.layers.ArcGISTiledMapServiceLayer(esriPublicServer, { opacity: 0.4 });
         //map.addLayer(baseMap);
 
@@ -64,20 +50,5 @@ $(document).ready(function () {
         //wmsLayer.setVisibleLayers([2]);
         //wmsLayer.setImageFormat("png");
         //map.addLayer(wmsLayer);
-
-        arcgisUtils.createMap("0187399cb40040f5948eb8051eddc780", "arcgis_map")
-
-        var resizeTimer;
-        dojo.connect(arcgis_map, 'onLoad', function (theMap) {
-            dojo.connect(dijit.byId('map'), 'resize', function () {
-                clearTimeout(resizeTimer);
-                resizeTimer = setTimeout(function () {
-                    map.resize();
-                    map.reposition();
-                }, 500);
-            });
-        });
     }
-
-    //dojo.addOnLoad(init);
 });
