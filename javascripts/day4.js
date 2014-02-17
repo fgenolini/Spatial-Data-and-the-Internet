f9199bc2f379feb694762320be333ad63695df9f
Francois Genolini <fgenolini@hotmail.com>
2014-02-17 12:06:27 +0000

    Clean-up

--- javascripts/day4.js
+++ javascripts/day4.js
@@ -1,8 +1,9 @@
 /*
 Day 4 of module A1136 Spatial Data and the Internet, Cranfield University (UK)
 
-The Aitana dataset will only work from within the Cranfield University LAN,
-as it uses a local private PC (not on the Internet) as ArcGIS Server
+The Aitana dataset was supplied by Cranfield University during the course week,
+then uploaded to ArcGIS online as shapefile, then a public web map was created.
+This script utilises the public web map
 */
 if (typeof jQuery === 'undefined') {
     document.write(unescape("%3Cscript src='javascripts/jquery-1.11.0.min.js' type='text/javascript'%3E%3C/script%3E"));
@@ -10,20 +11,21 @@ if (typeof jQuery === 'undefined') {
 
 $(document).ready(function () {
     var dojoConfig = { parseOnLoad: true };
-    var fgenoliniAitanaArcGisOnlineMapId = "e297c36a60424a0181ad64572ffaaaf9";
-    var arcgis_map;
     require([
         "esri/map",
         "esri/arcgis/utils",
         "esri/dijit/Legend",
         "dojo/domReady!"
     ], function (Map, arcgisUtils, Legend) {
+        var fgenoliniAitanaArcGisOnlineMapId = "e297c36a60424a0181ad64572ffaaaf9";
         var configOptions = {
             webmap: fgenoliniAitanaArcGisOnlineMapId,
             title: "",
             subtitle: "",
             sharingurl: "http://www.arcgis.com/sharing/content/items"
         };
+
+        // Proxy must be setup otherwise CORS will prevent browser from accessing data
         arcgisUtils.arcgisUrl = configOptions.sharingurl;
         esri.config.defaults.io.proxyUrl = "/proxy";
         var urlObject = esri.urlToObject(document.location.href);
@@ -33,7 +35,7 @@ $(document).ready(function () {
         }
 
         arcgisUtils.createMap(configOptions.webmap, "arcgis_map").then(function (response) {
-            arcgis_map = response.map;
+            var arcgis_map = response.map;
             var legend = new Legend({
                 map: arcgis_map,
                 layerInfos: (arcgisUtils.getLegendLayers(response))
@@ -42,27 +44,4 @@ $(document).ready(function () {
             legend.startup();
         });
     });
-
-    function init() {
-        // Base map showing cultural information
-        // Public ESRI base map layer
-        //var esriPublicServer = "http://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer";
-        //var baseMap = new esri.layers.ArcGISTiledMapServiceLayer(esriPublicServer, { opacity: 0.4 });
-        //map.addLayer(baseMap);
-
-        // ESRI ArcGIS Server located on the private LAN
-        // within Cranfield University, not a public server
-        // Aitana sample data set
-        //var cranfieldUniversityLocalServer = "http://ssw7311f.sims.cranfield.ac.uk:6080/arcgis/rest/services/SDI/SDI/MapServer";
-        //var cranfieldLayerOptions = {
-        //    opacity: 0.6
-        //};
-        //var layer = new esri.layers.ArcGISDynamicMapServiceLayer(cranfieldUniversityLocalServer,
-        //    cranfieldLayerOptions);
-        // WMS data from public server
-        //var wmsLayer = new esri.layers.WMSLayer("http://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/WMSServer");
-        //wmsLayer.setVisibleLayers([2]);
-        //wmsLayer.setImageFormat("png");
-        //map.addLayer(wmsLayer);
-    }
 });
