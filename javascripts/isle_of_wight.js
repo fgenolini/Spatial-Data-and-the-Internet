84c61abea0647f95aea97c039dbc3a01f4376e5e
François Génolini <fgenolini@hotmail.com>
2014-02-18 14:25:00 +0000

    Investigating Dojo

--- javascripts/isle_of_wight.js
+++ javascripts/isle_of_wight.js
@@ -1,8 +1,18 @@
 /*
 Assignment for module A1136 Spatial Data and the Internet, Cranfield University (UK)
  */
-if(typeof jQuery === 'undefined'){
+if (typeof jQuery === 'undefined') {
     document.write(unescape("%3Cscript src='javascripts/jquery-1.11.0.min.js' type='text/javascript'%3E%3C/script%3E"));
 }
+
 $(document).ready(function () {
+    require([
+        'isle_of_wight/soil'
+    ], function (myModule) {
+        soil.setText('greeting', 'Hello Dojo!');
+
+        setTimeout(function () {
+            myModule.restoreText('greeting');
+        }, 3000);
+    });
 });
