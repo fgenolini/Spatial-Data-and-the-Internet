054fcf24c1320dd111b118d3f2e448e64f59cb49
Francois Genolini <fgenolini@hotmail.com>
2014-02-20 11:30:24 +0000

    script

---
+++ javascripts/video.js
@@ -0,0 +1,7 @@
+if (typeof jQuery === 'undefined') {
+    document.write(unescape("%3Cscript src='javascripts/jquery-1.11.0.min.js' type='text/javascript'%3E%3C/script%3E"));
+}
+
+$(document).ready(function () {
+    $('#player').attr('src', http://youtu.be/FNDTVUWSLxE);
+});
